c674dfb10e1cdbfbcf718d6595f3c0d545d80e17
Jaeseung Lee <41176085+tkxkd0159@users.noreply.github.com>
2026-03-25 14:55:18 +0900

    Initialize spring boot

---
+++ src/main/java/com/example/opensearch_mcp/OpensearchMcpApplication.java
@@ -0,0 +1,13 @@
+package com.example.opensearch_mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpensearchMcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpensearchMcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/opensearch_mcp/OpensearchMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.opensearch_mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpensearchMcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
